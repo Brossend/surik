e40d17d922b03e317b83a4b89d68022f8d9bf76e
i.lyamtsev <ilyamtsev@c-and-u.co>
2026-03-11 21:45:54 +0500

    feat(app): init app

---
+++ app/app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="app-shell">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+
+    <div class="app-shell__glow app-shell__glow--top-left" />
+    <div class="app-shell__glow app-shell__glow--bottom-right" />
+  </div>
+</template>
+
+<style lang="scss" scoped src="./app.scss" />
---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  css: ['~/assets/styles/global.scss'],
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxtjs/i18n',
+    '@pinia/nuxt'
+  ]
+})
